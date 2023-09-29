9e6101682965759711d1571160b920ec11629abd
Yasemin Akın <akinnyaseminn3@gmail.com>
2023-09-29 15:13:58 +0300

    dependency-integration

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Test from "./test.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
 )
