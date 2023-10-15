3f56586a446b48aab8135e8c20eb93f4021be53b
Yasemin Akın <akinnyaseminn3@gmail.com>
2023-10-15 23:01:47 +0300

    Add D1

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Test from "./test.jsx";
+import Navbar from "./components/navbar.jsx";
+import '../flowbite.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Test />
+    <Navbar />
   </React.StrictMode>,
 )
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,15 +1,14 @@
+import flowbite from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
       "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [flowbite],
 }
-
