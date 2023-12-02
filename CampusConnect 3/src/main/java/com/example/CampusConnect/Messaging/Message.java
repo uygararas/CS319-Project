ff393324bbd7442cbba353af93c640bda2801685
keskinulas <ulaskeskin2003@hotmail.com>
2023-12-02 18:25:18 +0300

    created message class and config, changes to pom(dependency)

---
+++ CampusConnect 3/src/main/java/com/example/CampusConnect/Messaging/Message.java
@@ -0,0 +1,13 @@
+package com.example.CampusConnect.Messaging;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Message {
+
+
+    private String messageContents;
+}
