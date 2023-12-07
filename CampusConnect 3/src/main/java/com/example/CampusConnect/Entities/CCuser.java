ce6d10fbb2cd2a5997af7bc50b018474a9f84ffd
Uygar aras <uygararas3@gmail.com>
2023-12-07 17:41:01 +0300

    edit

--- CampusConnect 3/src/main/java/com/example/CampusConnect/Entities/CCuser.java
+++ CampusConnect 3/src/main/java/com/example/CampusConnect/Entities/CCuser.java
@@ -14,6 +14,7 @@ import java.util.UUID; // Import for generating unique verification token
 @Table(name = "CCusers")
 public class CCuser {
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
